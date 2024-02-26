d3ce98463da184d8738c4ebea24fc6d2e04fb00e
WesllenWorld <wesllen.2020111052@unicap.br>
2024-02-26 15:12:40 -0300

    Criando pastas do repositório

---
+++ python-dio/basico/conhecendo a linguagem/script.py
@@ -0,0 +1,12 @@
+class teste:
+    def __init__(self):
+        self.message = "Testado, agora sobe no github!"
+
+    def print_message(self):
+        print(self.message)
+
+# Criando uma instância da classe HelloWorld
+hello_world = teste()
+
+# Chamando o método print_message para imprimir a mensagem
+hello_world.print_message()
